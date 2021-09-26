dc5e8953b372776ce4b005f9875f8ddf3b2ad8a4
Rafa <Rafa@DESKTOP-QNIR2T8.home>
2021-09-26 18:23:31 -0300

    Primeiro Projeto

---
+++ src/com/rafael/utils/filas/integer/No.java
@@ -0,0 +1,13 @@
+package com.rafael.utils.filas.integer;
+
+public class No {
+
+	int dado;
+	No proximo;
+
+	@Override
+	public String toString() {
+		return "Dado: " + dado;
+	}
+
+}
---
+++ src/com/rafael/utils/filas/string/No.java
@@ -0,0 +1,13 @@
+package com.rafael.utils.filas.string;
+
+public class No {
+
+	String dado;
+	No proximo;
+
+	@Override
+	public String toString() {
+		return "Dado: " + dado;
+	}
+
+}
